707c9530b5d3729977b8d4edd8d4c92d602a68b4
SB <sb@SB-MacBook-Pro.local>
2020-02-20 01:11:31 -0800

    rapid api added and front end

---
+++ apiRequest.py
@@ -0,0 +1,26 @@
+import requests
+
+def getFutureFixture():
+
+	url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/league/524/next/10"
+
+	querystring = {"timezone":"America/Los_Angeles"}
+
+	headers = {
+	    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+	    'x-rapidapi-key': "65ff80534cmsh0d3a35298c91796p1dc48bjsna05653c2e9f4"
+	    }
+
+	response = requests.request("GET", url, headers=headers,params=querystring)
+	data = response.json()
+
+	fixtures = data['api']['fixtures']
+
+	fixture_list = []
+
+	for fixture in fixtures:
+	    fixture_list.append([fixture['event_date'],fixture['homeTeam']['team_name'],fixture['awayTeam']['team_name']])
+
+	return fixture_list
+
+
--- app.py
+++ app.py
@@ -6,6 +6,9 @@ from scipy.stats import poisson,skellam
 import pandas as pd
 from datetime import date
 from update import update_model
+from apiRequest import getFutureFixture
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 app = Flask(__name__)
 
@@ -13,7 +16,18 @@ app = Flask(__name__)
 #### Loading predict model ####
 ###############################
 
-predictor = pickle.load(open(os.path.join('predictor/plk_objects/predictor.plk'), 'rb'))
+# predictor = pickle.load(open(os.path.join('predictor/plk_objects/predictor.plk'), 'rb'))
+
+epl_1920 = pd.read_csv("http://football-data.co.uk/mmz4281/1920/E0.csv")
+epl_1920 = epl_1920[['HomeTeam','AwayTeam','FTHG','FTAG','FTR']]
+epl_1920 = epl_1920.rename(columns={'FTHG': 'HomeGoals', 'FTAG': 'AwayGoals','FTR':'Result'})
+
+goal_model_data = pd.concat([epl_1920[['HomeTeam','AwayTeam','HomeGoals']].assign(home=1).rename(
+            columns={'HomeTeam':'team', 'AwayTeam':'opponent','HomeGoals':'goals'}),
+           epl_1920[['AwayTeam','HomeTeam','AwayGoals']].assign(home=0).rename(
+            columns={'AwayTeam':'team', 'HomeTeam':'opponent','AwayGoals':'goals'})])
+
+poisson_model = smf.glm(formula="goals ~ home + team + opponent", data=goal_model_data, family=sm.families.Poisson()).fit()
 
 def simulate_match(foot_model, homeTeam, awayTeam, max_goals=10):
     home_goals_avg = foot_model.predict(pd.DataFrame(data={'team': homeTeam, 
@@ -26,44 +40,60 @@ def simulate_match(foot_model, homeTeam, awayTeam, max_goals=10):
     return(np.outer(np.array(team_pred[0]), np.array(team_pred[1])))
 
 
+future_fixtures = getFutureFixture()
+
+def changeClubName(matches):
+
+    for match in matches:
+        home = match[1]
+        away = match[2]
+
+        if match[1] == 'Manchester City':
+            match[1] = 'Man City'
+        elif match[1] == 'Manchester United':
+            match[1] = 'Man United'
+        elif match[1] == 'Sheffield Utd':
+            match[1] = 'Sheffield United'
+
+        if match[2] == 'Manchester City':
+            match[2] = 'Man City'
+        elif match[2] == 'Manchester United':
+            match[2] = 'Man United'
+        elif match[2] == 'Sheffield Utd':
+            match[2] = 'Sheffield United'
+            
+    return matches
+
+upcoming_fixtures = changeClubName(future_fixtures)
+print(upcoming_fixtures)
+
+
 
 ###############################
 ####  	Flask 			   ####
 ###############################
 @app.route("/")
 def index():
-	fixture = pd.read_csv("static/data/epl-2019-GMTStandardTime.csv")
+    data = []
 
-	today = date.today()
-	fixture['Date'] = pd.to_datetime(fixture['Date'])
-	future = fixture.loc[fixture['Date'] >= today]
-	homeTeam = future['Home Team'].iloc[:10]
-	awayTeam = future['Away Team'].iloc[:10]
-	dates = future['Date'].iloc[:10]
+    for index in upcoming_fixtures:
+        home = index[1]
+        away = index[2]
+        result = simulate_match(poisson_model, home, away, max_goals=10)
 
-	upcoming_ten_matches = pd.DataFrame(data = {'Date' : dates, 'HomeTeam': homeTeam, 'AwayTeam': awayTeam})
+        home_prop = np.round(np.sum(np.tril(result,-1))*100)
+        draw_prop = np.round(np.sum(np.diag(result))*100)
+        away_prop = np.round(np.sum(np.triu(result,1))*100)
+        game = {
+            'match':index[0]+' '+index[1]+' vs '+index[2],
+            'home':home_prop,
+            'draw':draw_prop,
+            'away':away_prop
+        }
+        data.append(game)
 
-	matches = []
-	home_prop = []
-	draw_prop = []
-	away_prop = []
-
-	for index, row in upcoming_ten_matches.iterrows():
-	    home = row['HomeTeam']
-	    away = row['AwayTeam']
-
-	    match = home + '_' + away
-	    matches.append(match)
-	    
-	    result = simulate_match(predictor, home, away, max_goals=10)
-	    home_prop.append(np.sum(np.tril(result, -1)))
-	    draw_prop.append(np.sum(np.diag(result)))
-	    away_prop.append(np.sum(np.triu(result, 1)))
-	    
-	result_pd = pd.DataFrame({'matches': matches, 'home': home_prop, 'draw': draw_prop, 'away': away_prop})
-
-
-	return render_template("index.html", data = model, hometeams = hometeam, awayteams= awayteam)
+    print(data)
+    return render_template("index.html", data = data)
 
 
 if __name__ == "__main__":
